e8b251047c9acb83d7653dd393dde007bbabddb6
xuorig <mgiroux0@gmail.com>
2021-07-02 09:40:57 -0400

    CLI formats

--- src/cli/diff.rs
+++ src/cli/diff.rs
@@ -1,4 +1,5 @@
 use crate::diff::diff_json;
+use clap::arg_enum;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -11,6 +12,19 @@ pub struct DiffCommand {
     /// The head OpenAPI description to be compared with base
     #[structopt(short = "h", long = "head-file", parse(from_os_str))]
     head: PathBuf,
+
+    /// Output format for the diff
+    #[structopt(short = "f", long = "format", default_value = "default", possible_values = &Format::variants(), case_insensitive = true)]
+    format: Format,
+}
+
+arg_enum! {
+    #[derive(Debug)]
+    pub enum Format {
+        Default,
+        JSON,
+        YAML,
+    }
 }
 
 impl DiffCommand {
@@ -18,11 +32,23 @@ impl DiffCommand {
         let res = diff_json(self.base.clone(), self.head.clone());
 
         match res {
-            Ok(diff) => {
-                let json =
-                    serde_json::to_string_pretty(&diff).expect("Could not serialize diff to JSON");
-                println!("{}", json)
-            }
+            Ok(diff) => match self.format {
+                Format::JSON => {
+                    let json = serde_json::to_string_pretty(&diff)
+                        .expect("Could not serialize diff to JSON");
+                    println!("{}", json);
+                }
+                Format::YAML => {
+                    let yaml =
+                        serde_yaml::to_string(&diff).expect("Could not serialize diff to YAML");
+                    println!("{}", yaml);
+                }
+                Format::Default => {
+                    let json = serde_json::to_string_pretty(&diff)
+                        .expect("Could not serialize diff to JSON");
+                    println!("{}", json);
+                }
+            },
             Err(e) => {
                 eprintln!("Error: {}", e)
             }
