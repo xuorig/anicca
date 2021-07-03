57b245ba9071d7a640694f90c93fabca38d1ddee
xuorig <mgiroux0@gmail.com>
2021-07-03 09:25:55 -0400

    request body

--- src/diff/request_body.rs
+++ src/diff/request_body.rs
@@ -1,8 +1,13 @@
+use super::content::ContentDiff;
 use openapiv3::{ReferenceOr, RequestBody};
 use serde::Serialize;
 
 #[derive(Debug, Serialize)]
-pub struct RequestBodyDiff {}
+pub struct RequestBodyDiff {
+    added: Option<ReferenceOr<RequestBody>>,
+    removed: Option<ReferenceOr<RequestBody>>,
+    content_changed: Option<ContentDiff>,
+}
 
 impl RequestBodyDiff {
     pub fn has_changes(&self) -> bool {
@@ -13,6 +18,56 @@ impl RequestBodyDiff {
         base: &Option<ReferenceOr<RequestBody>>,
         head: &Option<ReferenceOr<RequestBody>>,
     ) -> Self {
-        Self {}
+        match base {
+            Some(base) => match head {
+                Some(head) => {
+                    let derefed_base = match &base {
+                        ReferenceOr::Item(bc) => bc,
+                        ReferenceOr::Reference { reference: _ } => {
+                            panic!("$ref not supported yet");
+                        }
+                    };
+
+                    let derefed_head = match &head {
+                        ReferenceOr::Item(pi) => pi,
+                        ReferenceOr::Reference { reference: _ } => {
+                            panic!("$ref not supported yet");
+                        }
+                    };
+
+                    let mut diff = Self {
+                        added: None,
+                        removed: None,
+                        content_changed: None,
+                    };
+
+                    let content_diff =
+                        ContentDiff::from_content(&derefed_base.content, &derefed_head.content);
+
+                    if content_diff.has_changes() {
+                        diff.content_changed = Some(content_diff);
+                    }
+
+                    diff
+                }
+                None => Self {
+                    added: None,
+                    removed: Some(base.clone()),
+                    content_changed: None,
+                },
+            },
+            None => match head {
+                Some(head) => Self {
+                    added: Some(head.clone()),
+                    removed: None,
+                    content_changed: None,
+                },
+                None => Self {
+                    added: None,
+                    removed: None,
+                    content_changed: None,
+                },
+            },
+        }
     }
 }
